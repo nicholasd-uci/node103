a546fa571dbf09d01b4b0eae2a05eabe72f2ca5c
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-19 09:57:25 -0800

    set up node notes for all users

---
+++ npm.js
@@ -0,0 +1,16 @@
+//      ** "initializing with NPM" (Node Package Manager) **
+//  **command 1. create a .gitignore
+//  **command 2. npm init -y
+//  **command 3. type npm package => "npm i chalk" or "npm i inquirer"
+//  **command 4. repeat process for all new repositories
+
+//  ** if node_mods are not current in explorer. Run in Terminal ===   npm i  
+//  ** Check package-lock for details
+
+// ** Documentation for all your NPMs can be found in the "Usage Section"
+
+
+// const chalk = require('chalk')
+
+
+// console.log(chalk.blue('Hello World!'))
