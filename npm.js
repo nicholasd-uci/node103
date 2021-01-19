8c52c137cb4718406f92cd8b10f82bb869bbad47
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-19 10:46:26 -0800

    updated npm.js notes

--- npm.js
+++ npm.js
@@ -1,12 +1,11 @@
 //      ** "initializing with NPM" (Node Package Manager) **
+
 //  **command 1. npm init -y
 //  **command 2. type npm package => "npm i chalk" or "npm i inquirer"
 //  **command 3. create a .gitignore
 //  **command 4. repeat process for all new repositories
 
-//  ** if node_mods are not current in explorer. Run in Terminal ===   npm i  
-//  ** Check package-lock for details
-
+//  **HINT** if you are pulling from another repo use "npm i" 
 // ** Documentation for all your NPMs can be found in the "Usage Section"
 
 const chalk = require('chalk')
