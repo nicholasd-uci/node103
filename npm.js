9cd74ca5f28156b05a4f0c02c37bd67a1b7a6aba
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-20 08:58:50 -0800

    included notes backinto npm.js

--- npm.js
+++ npm.js
@@ -1,3 +1,12 @@
+//      ** "initializing with NPM" (Node Package Manager) **
+
+//  **command 1. npm init -y
+//  **command 2. type npm package => "npm i chalk" or "npm i inquirer"
+//  **command 3. create a .gitignore
+//  **command 4. repeat process for all new repositories
+
+//  **HINT** if you are pulling from another repo use "npm i" 
+
 // Chalk NPM 
 const chalk = require('chalk')
 const log = console.log
