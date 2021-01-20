29f81b2eceecb4c5c19eb8a6e959bcd2b6a7b233
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-19 22:11:04 -0800

    updated app.js

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-
-
-
-
-
-// inquirer is another npm that helps you inquir a set of prompts that can be found in its "Documentation"
-// This is setting inquirer to be required, in oder for the Documentation to render
 const inquirer = require('inquirer')
 
 
-// this was added after we did "const inquirer". This Functions lets you have a round of question repeat until the end
 // reserve function === its a function that calls itself 
 const question = () => {
 
@@ -29,9 +21,7 @@ const question = () => {
         ])
         // .then will always be "res =>"
         .then(res => {
-        // if I wanted the just the name in the prompts it would be (res.name || res.choice)
             console.log(res.name, res.choice)
         })
-        // .catch will always be "err =>"
         .catch(err => console.log(err))
 }
