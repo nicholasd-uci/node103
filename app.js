4c8444888ed1729b96011891870ee84910da1198
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-20 09:21:31 -0800

    tested app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer')
 
-
 // reserve function === its a function that calls itself 
 const question = () => {
 
@@ -15,13 +14,25 @@ const question = () => {
             {
                 type: 'list',
                 name: 'choice',
-                message: 'Pick a number',
-                choices: ['one', 'two', 'three']
-            }     
+                message: 'What is your favorite Pokemon?',
+                choices: ['Charizard', 'MewTwo', 'Pikachu']
+            },
+            {
+                type: 'confirm',
+                name: 'confirm',
+                message: 'Are there more than 100 pokemon?'
+            }      
         ])
         // .then will always be "res =>"
         .then(res => {
-            console.log(res.name, res.choice)
+            console.log(
+                ` Okay awesome so...
+                  Your name is ${res.name}
+                  Your favorite pokemon is ${res.choice}
+                  If you answered YES, then your correct! ==> Your answer ${res.confirm}
+                  `)
         })
         .catch(err => console.log(err))
 }
+
+question()
